aa19c6268e9366055ee0c1869abd2307608d5849
serbo <petarserbo@gmail.com>
2021-04-15 16:38:06 +0300

    1.1 version

--- Stocks_App/controllers/DetailViewController.swift
+++ Stocks_App/controllers/DetailViewController.swift
@@ -8,24 +8,24 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    
     @IBOutlet weak var tickerDetailLabel: UILabel!
     @IBOutlet weak var nameDetailLabel: UILabel!
     @IBOutlet weak var changeDetailLabel: UILabel!
     @IBOutlet weak var priceDetailLabel: UILabel!
     @IBOutlet weak var sixMPriceDetail: UILabel!
     @IBOutlet weak var oneYPriceDetail: UILabel!
-   
+    
     var stocksItem: Stocks!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configure()
-
+        
         // Do any additional setup after loading the view.
     }
     
-   private func configure() {
+    private func configure() {
         tickerDetailLabel.text = stocksItem.symbol
         nameDetailLabel.text = stocksItem.fullName
         priceDetailLabel.text = stocksItem.price
--- Stocks_App/controllers/ViewController.swift
+++ Stocks_App/controllers/ViewController.swift
@@ -25,7 +25,7 @@ class ViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-
+        
     }
     
     private func loadData () {
